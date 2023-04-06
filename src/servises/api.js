ee91c34e82a3b8e688a0e5fd785510bcf5c24177
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-04-06 18:23:00 +0300

    add env

--- src/servises/api.js
+++ src/servises/api.js
@@ -1,5 +1,6 @@
-// const { REACT_APP_BASE_URL } = process.env;
+const { REACT_APP_BASE_URL } = process.env;
 // const BASE_URL = 'https://petly-brs3.onrender.com/api';
 // const BASE_URL = 'http://localhost:3001/api';
-const BASE_URL = 'https://pets-support-back.onrender.com/api';
+// const BASE_URL = 'https://pets-support-back.onrender.com/api';
+const BASE_URL = REACT_APP_BASE_URL;
 export default BASE_URL;
